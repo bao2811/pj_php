d0fa2fcbb2e8673ddae0ed343fcba4949f4e098e
Hoàng Bảo <hoangbao28112005@gmail.com>
2025-09-12 22:58:07 +0700

    .

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Data } from "@/components/Data";
+
+export default function Home() {
+  return (
+    <main>
+      <h1 className="text-3xl font-bold underline">Welcome to Note App!</h1>
+      <Data />
+    </main>
+  );
+}
